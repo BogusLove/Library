06fa456e3a06309af55e70d238f6420bbd8242a8
BogusLove <boguscompany@ukr.net>
2017-12-19 22:24:24 +0200

    test add

--- test/CRUD_test.js
+++ test/CRUD_test.js
@@ -7,26 +7,26 @@ const testCRUD = new crud('test');
 
 chai.use(chaiAsPromised);
 
-// describe('getAll', function() {
-//   it('should return Promise type', function() {
-//     expect(testCRUD.getAll().then).to.be.a('function');
-//   });
-// });
+describe('getAll', function() {
+  it('should return Promise type', function() {
+    expect(testCRUD.getAll().then).to.be.a('function');
+  });
+});
 
-// describe('add', function() {
-//   it('should return equal string', function() {
-//     expect(testCRUD.add().then).to.be.a('function');
-//   });
-// });
+describe('add', function() {
+  it('should return equal string', function() {
+    expect(testCRUD.add().then).to.be.a('function');
+  });
+});
 
-// describe('update', function() {
-//   it('should return equal string', function() {
-//     expect(testCRUD.update([]).then).to.be.a('function');
-//   });
-// });
+describe('update', function() {
+  it('should return equal string', function() {
+    expect(testCRUD.update([]).then).to.be.a('function');
+  });
+});
 
-// describe('delete', function() {
-//   it('should return equal string', function() {
-//     expect(testCRUD.delete().then).to.be.a('function');
-//   });
-// });
+describe('delete', function() {
+  it('should return equal string', function() {
+    expect(testCRUD.delete().then).to.be.a('function');
+  });
+});
